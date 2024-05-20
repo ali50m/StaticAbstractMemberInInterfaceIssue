a941c11846a09853f222b56a7b8eddb3c1847259
liu <liu@book16>
2024-05-20 10:06:16 +0800

    first commit

---
+++ StaticAbstractMemberInInterfaceIssue/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// these line works!
+IFoo foo = new Foo();
+Foo.GetCount();
+
+var services = new ServiceCollection();
+services.AddSingleton<IFoo, Foo>(); // why this line can not compile?
+
+// error message:
+// The interface 'IFoo' type cannot be used as a type argument for type parameter 'TService'.
+// The interface has static abstract members without implementations: int IFoo.GetCount()
+
+public interface IFoo
+{
+    static abstract int GetCount();
+}
+
+public class Foo : IFoo
+{
+    public static int GetCount()
+    {
+        return 1;
+    }
+}
